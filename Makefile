64421a4eb28c72c8cb5bdb382591dba45aca7d27
taaayan3 <andrea.aymon@swisscom.com>
2018-10-17 15:34:49 +0200

    fix issues

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ glide:
 start-mysql:
 	docker run --name smartdevdb \
 		-e MYSQL_ROOT_PASSWORD=blibb \
-		-e MYSQL_DATABASE=weather_db \
+		-e MYSQL_DATABASE=smartdevdb \
 		-e MYSQL_USER=blubb \
 		-e MYSQL_PASSWORD=blabb \
 		-p "3306:3306" \
@@ -31,8 +31,8 @@ start-mysql:
 	scripts/db_setup.sh
 
 stop-mysql:
-	docker kill weatherdb || true
-	docker rm -f weatherdb || true
+	docker kill smartdevdb || true
+	docker rm -f smartdevdb || true
 
 test:
 	GOARCH=amd64 GOOS=linux go test $$(go list ./... | grep -v /vendor/)
--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -16,9 +16,9 @@ source .env_mysql
 echo "setting up weatherdb ..."
 sleep 5
 
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < lib/database/migrations/mysql/001_weatherdb_sensor_tables.up.sql
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < lib/database/migrations/mysql/002_weatherdb_setup_sensors.up.sql
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < lib/database/migrations/mysql/003_weatherdb_config_table.up.sql
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < _fixtures/migration.sql
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < _fixtures/sensors.sql
-mysql --host=127.0.0.1 --user=blubb --password=blabb --database=weather_db -v < _fixtures/values.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < lib/database/migrations/mysql/001_weatherdb_sensor_tables.up.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < lib/database/migrations/mysql/002_weatherdb_setup_sensors.up.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < lib/database/migrations/mysql/003_weatherdb_config_table.up.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < _fixtures/migration.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < _fixtures/sensors.sql
+mysql --host=127.0.0.1 --user=blubb --password=blabb --database=smartdevdb -v < _fixtures/values.sql
