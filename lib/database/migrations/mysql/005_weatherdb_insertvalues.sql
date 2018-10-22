b30538d1340b8a77a5dfed4541a33a608292116f
taaayan3 <andrea.aymon@swisscom.com>
2018-10-22 09:30:05 +0200

    add some testvalues

---
+++ lib/database/migrations/mysql/005_weatherdb_insertvalues.sql
@@ -0,0 +1,16 @@
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-22 20:06:55', '10', '24');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('018-10-20 20:06:55','7', '2');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-20 21:07:55','7', '3');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-20 21:07:55','10', '25');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-20 21:08:55', '7', '4');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-20 21:08:55','10', '26');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES(i'2018-10-20 22:07:55','7', '5');
+INSERT INTO sensor_data (timestamp, fk_sensor_id, value)
+VALUES('2018-10-20 22:07:55','10', '22');
